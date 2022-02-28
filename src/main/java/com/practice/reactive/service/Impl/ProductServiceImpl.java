5adf4e43001238e71e2692a7b955f237839af925
PavanMane1995 <letscode1995@gmail.com>
2022-03-01 00:25:10 +0530

    Created API's to get and save product in mongodb

--- src/main/java/com/practice/reactive/service/Impl/ProductServiceImpl.java
+++ src/main/java/com/practice/reactive/service/Impl/ProductServiceImpl.java
@@ -5,8 +5,9 @@ import com.practice.reactive.entity.Product;
 import com.practice.reactive.repository.ProductRepository;
 import com.practice.reactive.service.ProductService;
 import com.practice.reactive.utils.AppUtils;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -18,8 +19,11 @@ public class ProductServiceImpl implements ProductService {
     ProductRepository productRepository;
 
     public Flux<ProductDto> getAllProducts(){
+        Query q = new Query();
+        Criteria a = Criteria.where("name").is("pavan");
+        q.addCriteria(a);
         return productRepository
-                .findAll()
+                .findAll().log()
                 .map(AppUtils::entityToDto);
 
     }
@@ -30,8 +34,11 @@ public class ProductServiceImpl implements ProductService {
                 .map(AppUtils::entityToDto);
     }
 
-    public void saveProduct(ProductDto pDto){
-        Product p = AppUtils.dtoToEntity(pDto);
-        productRepository.save(p).log();
+    public Mono<ProductDto> saveProduct(Mono<ProductDto> pDto){
+       return pDto.map(AppUtils::dtoToEntity)
+                .flatMap(productRepository::insert)
+                .map(AppUtils::entityToDto)
+               .onErrorResume(e -> {return Mono.just(new ProductDto());});
+
     }
 }
